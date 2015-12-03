b367f355a13068b403ec3a9b001f7a4ac45ac7e5
pro-link <p@pro-link.pl>
2015-12-03 09:08:36 +0100

    Update cookie.js

--- cookie.js
+++ cookie.js
@@ -3,8 +3,7 @@ onload = function(){
     var newdiv = document.createElement('div');
     var divIdName = 'mycookie';
     newdiv.setAttribute('id',divIdName);
-	newdiv.style.width = "100%";
-    newdiv.style.height = "50px";
+    newdiv.style.width = "100%";
     newdiv.style.padding = "5px 0";
     newdiv.style.left = "0px";
     newdiv.style.bottom = "0px";
@@ -15,8 +14,7 @@ onload = function(){
     newdiv.style.borderTop = "1px solid #444444";
     newdiv.style.boxShadow = "5px 0px 40px #000000";
 	web=window.location.hostname;
-    newdiv.innerHTML = '<div style="width: 960px; margin: 0 auto;"><p style="float: left; text-align: left; width: 890px; line-height: 18px; font-size: 11px; color: #3f3f3f; font-family: Tahoma;">Serwis u&#380;ywa plik&#243;w cookie. Przeczytaj wi&#281;cej o celu ich wykorzystania i <a style="font-size: 11px; color: #3f3f3f; font-family: Tahoma; text-decoration: underline;" rel="nofollow" href="http://www.politykacookie.com.pl/cookie.php?web='+ web +'" target="_new">mo&#380;liwo&#347;ci zmiany ustawie&#324; cookies w Twojej przegl&#261;darce</a>. Korzystaj&#261;c z naszego serwisu wyra&#380;asz zgod&#281; na u&#380;ywanie cookies, zgodnie z ustawieniami Twojej przegl&#261;darki.</p> <span id="close" style="cursor: pointer; display: block; float: right; padding: 10px 15px; margin: 7px 0 0 0; background: #444; color: #fff; font-weight: bold; font-size: 13px; font-family: Tahoma;" href="" onclick="addcookie();" >X</span><div style="clear: both;"></div></div>';
-
+    newdiv.innerHTML = '<div style="width: 100%; margin: 0 0 0 5px;"><p style="float: left; text-align: left; width: 90%; line-height: 18px; font-size: 11px; color: #3f3f3f; font-family: Tahoma;">Serwis u&#380;ywa plik&#243;w cookie. Przeczytaj wi&#281;cej o celu ich wykorzystania i <a style="font-size: 11px; color: #3f3f3f; font-family: Tahoma; text-decoration: underline;" rel="nofollow" href="http://www.politykacookie.com.pl/cookie.php?web='+ web +'" target="_new">mo&#380;liwo&#347;ci zmiany ustawie&#324; cookies w Twojej przegl&#261;darce</a>. Korzystaj&#261;c z naszego serwisu wyra&#380;asz zgod&#281; na u&#380;ywanie cookies, zgodnie z ustawieniami Twojej przegl&#261;darki.</p> <span id="close" style="cursor: pointer; display: block; float: right; padding: 5px 7px; margin: 5px 0 0 0; background: #444; color: #fff; font-weight: bold; font-size: 13px; font-family: Tahoma;" href="" onclick="addcookie();" >X</span><div style="clear: both;"></div></div>';
 	document.body.insertBefore(newdiv, document.body.lastChild);
 
     jestwc=gC('powiadom');
